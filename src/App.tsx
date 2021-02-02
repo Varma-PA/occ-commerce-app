3f963db776da21d252e3c7504dfda076883c5cb6
Jatin <jatinmadaan9882@gmail.com>
2021-02-02 17:47:43 +0530

    Header added & Now Cards fetching data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import ProductCard from './components/ProductCard/ProductCard';
+import { useEffect, useState } from 'react'
+
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(function() {
+    (async function getProducts() {
+      await fetch("https://fakestoreapi.com/products?limit=10")
+      .then(res => res.json()).then(res => setProducts(res));
+    })()
+  }, []);
+
   return (
     <div className="App">
-      ECommerce Cart
+      <Header />
+      <div className="d-flex flex-wrap"> { products.map(product => <ProductCard  product={product}/>) } </div>
     </div>
   );
 }
