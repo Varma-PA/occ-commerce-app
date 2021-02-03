3b931b1cc15dda05aef73cb193f7e639da2939de
Achyuth Varma <achyuth.varma1@gmail.com>
2021-02-03 11:14:14 +0530

    Added basic route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 function Header() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light shadow mb-4">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Shopping Cart</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow mb-4">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Shopping Cart
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* <button className="btn btn-outline-success" type="submit" onClick={}>
+          Cart
+        </button> */}
+
+        <Link to="/cart">
+          <button className="btn btn-outline-success">Cart</button>
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
---
+++ src/pages/cart-page/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CartPage() {
+  return (
+    <div>
+      <h2>This is the Cart Page</h2>
+    </div>
+  );
+}
+
+export default CartPage;
